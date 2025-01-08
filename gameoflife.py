3c02d1fa8cb852cc1616a579765f08982ef65f13
mirabelallah <blanchelallah@gmail.com>
2025-01-08 23:11:54 +0100

    2e fonction: determine le nombre de voisin vivants d'une case

--- gameoflife.py
+++ gameoflife.py
@@ -1,7 +1,7 @@
 import random
 
 
-def initialiser_grille(nb_lignes, nb_colonnes):
+def grille(nb_lignes, nb_colonnes):   # on initialise la grille
     grille = []
     for _ in range(nb_lignes):
         ligne = []
@@ -11,6 +11,19 @@ def initialiser_grille(nb_lignes, nb_colonnes):
     return grille
 
 
-grille = initialiser_grille(5, 5)
-for ligne in grille:
+grillejeu = grille(5, 5)
+for ligne in grillejeu:
     print(ligne)
+
+
+def voisinV(grille, x, y):  # fonction pour determiner le nbre de voisins vivants 
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]  # on verifie toutes les cellules adjacentes a une cellule donnée
+    nbre_voisin_vivants = 0
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy 
+        if 0 <= nx < len(grille) and 0 <= ny < len(grille[0]):
+            nbre_voisin_vivants += grille[nx][ny]
+    return nbre_voisin_vivants
+
+
+
